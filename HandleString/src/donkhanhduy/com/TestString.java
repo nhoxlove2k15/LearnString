a66167d9ef6c2c86fa627e8960aece1da1866bfb
admin <admin@192.168.91.1>
2019-12-27 11:21:02 +0700

    Author : Duy Don Date : 27/12/2019 Last Update : 27/12/2019

--- HandleString/src/donkhanhduy/com/TestString.java
+++ HandleString/src/donkhanhduy/com/TestString.java
@@ -3,10 +3,40 @@ package donkhanhduy.com;
 public class TestString {
 
 	public static void main(String[] args) {
-		System.out.println("Heollo!!");
-		System.out.println("Duy");
 
-		//
+/////////////////////////////////////
+String a = "D:/Desktop:/Closer.mp3" ;
+int index = a.lastIndexOf("/");
+int index2 = a.lastIndexOf(".");
+
+
+String temp = a.substring(index+1, index2);
+System.out.println(temp);
+////////////////////
+
+a = "donguyennhathan@gmail" ;
+String [] b =  a.split("@");
+System.out.println(b[0]);
+
+
+index = a.lastIndexOf("@") ;
+System.out.println(a.substring(0 , index));
+a = "donguyennhathan@gmail.com.vn" ;
+index = a.lastIndexOf("@") ; 
+index2 = a.indexOf(".") ;
+System.out.println(a.substring(index+1,index2));
+//////////////
+a = "01-donguyenhathan-class1k";
+String [] c =  a.split("-");
+for (int i = 0 ; i < c.length ; i++)
+{
+System.out.println(c[i]);
+}
+/*
+ Viet ham toi uu chuoi khi nguoi dung nhap 1 ten bat ky 
+ 1.xoa
+ */
+
 	}
 
 }
